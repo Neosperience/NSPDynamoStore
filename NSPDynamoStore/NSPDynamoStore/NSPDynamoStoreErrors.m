32742f82ea5ff4b7c6c7f847f8dda9a29ffe03a8
Janos Tolgyesi <janos.tolgyesi@gmail.com>
2015-04-03 11:22:04 +0200

    Propagate errors to persistent store API

---
+++ NSPDynamoStore/NSPDynamoStore/NSPDynamoStoreErrors.m
@@ -0,0 +1,24 @@
+//
+//  NSPDynamoStoreErrors.m
+//  NSPDynamoStore
+//
+//  Created by Janos Tolgyesi on 03/04/15.
+//  Copyright (c) 2015 Neosperience SpA. All rights reserved.
+//
+
+#import "NSPDynamoStoreErrors.h"
+
+NSString* const NSPDynamoStoreErrorDomain = @"com.neosperience.NSPDynamoStoreErrorDomain";
+
+@implementation NSPDynamoStoreErrors
+
++(NSError *)fetchErrorWithUnderlyingError:(NSError *)error
+{
+    NSMutableDictionary *userInfo = [NSMutableDictionary dictionary];
+    userInfo[NSLocalizedDescriptionKey] = NSLocalizedStringFromTable(@"Failed to execute database request.", @"NSPDynamoStore", @"fetch error description");
+    userInfo[NSLocalizedFailureReasonErrorKey] = NSLocalizedStringFromTable(@"DynamoDB returned an error.", @"NSPDynamoStore", @"fetch error reason");
+    userInfo[NSUnderlyingErrorKey] = error;
+    return [NSError errorWithDomain:NSPDynamoStoreErrorDomain code:NSPDynamoStoreFetchErrorCode userInfo:userInfo];
+}
+
+@end
