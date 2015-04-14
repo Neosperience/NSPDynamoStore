1c4ff98dafc753eeb1bbdf216a623386163ecc6d
Janos Tolgyesi <janos.tolgyesi@gmail.com>
2015-04-14 17:18:29 +0200

    Support embedded JSON in Dynamo attributes as NSKeyedArchiver data archives

--- NSPDynamoStore/NSPExampleItem.h
+++ NSPDynamoStore/NSPExampleItem.h
@@ -2,7 +2,7 @@
 //  NSPExampleItem.h
 //  NSPDynamoStore
 //
-//  Created by Janos Tolgyesi on 03/04/15.
+//  Created by Janos Tolgyesi on 14/04/15.
 //  Copyright (c) 2015 Neosperience SpA. All rights reserved.
 //
 
@@ -23,7 +23,10 @@
 @property (nonatomic, retain) NSString * photoURL;
 @property (nonatomic, retain) NSString * tel;
 @property (nonatomic, retain) NSString * url;
-@property (nonatomic, retain) NSPExamplePerson *person;
+@property (nonatomic, retain) NSData * elements;
 @property (nonatomic, retain) NSPExampleCategory *category;
+@property (nonatomic, retain) NSPExamplePerson *person;
+
+-(id)elementsAsObject;
 
 @end
